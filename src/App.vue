8cd7e850521574dc90be1399c80c6f093f4e3b71
HbugLubas <harry.lubas@bellevuecollege.edu>
2019-02-04 11:13:13 -0800

    minor tweaks

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <BookListFilter msg="Book List" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import BookListFilter from "./components/BookList_Filter.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    BookListFilter
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
