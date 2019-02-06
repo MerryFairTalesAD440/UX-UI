dfd143cccfb9883efb4f43e181828b2ac3c8f7b1
John Laveta <lavetajm@gmail.com>
2019-02-05 19:09:08 -0800

    Cookie Read/Write/Delete Example

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Click on the buttons below to test Cookies" />
+    <ul>
+      <li> <!-- setCookie's values currently hard coded -->
+        <Button v-on:click="setCookie('sasToken', 'asdkjAKJHSajAS21983', 5)">Create Cookie</Button>
+      </li>
+      <li>
+        <Button v-on:click="getCookie">View Cookie</Button>
+      </li>
+      <li>
+        <Button v-on:click="deleteCookie">Erase Cookie</Button>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -11,7 +21,30 @@ import HelloWorld from "./components/HelloWorld.vue";
 export default {
   name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+  },
+  //Define methods used for the cookie test buttons.
+  methods: {
+    //setCookie will write a cookie based on the button input, currently with
+    //placeholder data
+    setCookie : function(sasToken, sasValue, expDays) {
+      console.log('Set Cookie');
+      var d = new Date();
+      d.setTime(d.getTime() + (expDays*24*60*60*1000));
+      var expireDate = "expires="+ d.toUTCString();
+      document.cookie = sasToken + "=" + sasValue + ";" + expireDate + ";path=/";
+    },
+    //getCookie method retrieves data written to the cookie
+    getCookie : function() {
+      console.log('Get Cookie');
+      var x = document.cookie;
+      alert(x);
+    },
+    //deleteCookie deletes the cookie by setting its expiration date in the past
+    deleteCookie : function() {
+      console.log('Delete Cookie')
+      document.cookie = "sasToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+    }
   }
 };
 </script>
@@ -25,4 +58,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+ul {
+  list-style-type: none;
+}
+
+li {
+  margin-bottom: 20px;
+}
+
 </style>
