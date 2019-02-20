2e2942ab523837292b00f98bbe3819e3c883382d
dnlbell <dnl.bell@gmail.com>
2019-02-20 13:19:35 -0800

    trying to break tests

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,7 +3,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "new message";
+    const msg = "wrong message";
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     });
