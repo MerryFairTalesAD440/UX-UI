be7904ba94cdfcc72ee5c656fe9a0fd8f7048155
dnlbell <dnl.bell@gmail.com>
2019-02-25 11:15:10 -0800

    removing /

--- travisCi/uploadDev.js
+++ travisCi/uploadDev.js
@@ -147,7 +147,7 @@ async function execute() {
   console.log("Containers:");
   await showContainerNames(aborter, serviceURL);
 
-  await clearJsBlobs(aborter, containerURL, pipeline, serviceURL.url + "/" + containerName + "/");
+  await clearJsBlobs(aborter, containerURL, pipeline, serviceURL.url + containerName + "/");
 
   for(let file of files){
     await uploadLocalFile(aborter, containerURL, file);
