07f60a6d7791c85782d0778028ffedaad487cc03
Jeremiah Smith <jsmit028@seattlecentral.edu>
2019-02-26 20:11:44 -0800

    i also had to update the vue template compiler

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
     <NavBar />
-    <BookListFilter msg="Book List" />
+    <BookList />
+    <!-- <BookListFilter msg="Book List" /> -->
   </div>
 </template>
 
 <script>
 import BookListFilter from "./components/BookListFilter.vue";
 import NavBar from "./components/NavBar.vue";
+import BookList from "./components/BookList.vue";
 
 export default {
   name: "app",
   components: {
     BookListFilter,
-    NavBar
+    NavBar,
+    BookList
   }
 };
 </script>
