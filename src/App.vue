a67a2e2b8ec741c720263f555d2069fa187f3aa6
Jeremiah Smith <jsmit028@seattlecentral.edu>
2019-02-28 16:35:28 -0800

    Jeremiahbranch (#39)
    * Broke up the admin portal page into two components: NavBar.vue and BookList.Vue .  I had to import vue-bootstrap into the project. I had to import vue-bootrap on the main.js file.              For the time being , I commented out the BookListFilter message on the App.vue file, in order to show the admin portal page rendering.     "<!-- <BookListFilter msg="Book List" /> -->"      I will integrate the BookListFilter into the current design.
    
    * i also had to update the vue template compiler

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
-    <BookListFilter msg="Book List" />
+    <NavBar />
+    <BookList />
+    <!-- <BookListFilter msg="Book List" /> -->
   </div>
 </template>
 
 <script>
 import BookListFilter from "./components/BookListFilter.vue";
+import NavBar from "./components/NavBar.vue";
+import BookList from "./components/BookList.vue";
 
 export default {
   name: "app",
   components: {
-    BookListFilter
+    BookListFilter,
+    NavBar,
+    BookList
   }
 };
 </script>
