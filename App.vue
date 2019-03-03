a074339c682cef1c376a29686eb6be2c755ea461
ncobb01 <34530495+ncobb01@users.noreply.github.com>
2019-03-02 16:07:23 -0800

    Add files via upload
    Updated using component format

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <NavBar />
+    <BookList />
+    <!-- <BookListFilter msg="Book List" /> -->
+  </div>
+</template>
+
+<script>
+import BookListFilter from "./components/BookListFilter.vue";
+import NavBar from "./components/NavBar.vue";
+import BookList from "./components/BookList.vue";
+import AddBook from "./components/AddBook.vue";
+
+export default {
+  name: "app",
+  components: {
+    BookListFilter,
+    NavBar,
+    BookList,
+    AddBook
+  }
+};
+</script>
+
+<style>
+
+</style>
