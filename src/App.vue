9c729aadd0eb0cab4192ec9e58a1a629ff1560d8
HbugLubas <harry.lubas@bellevuecollege.edu>
2019-03-02 16:57:31 -0800

    edited router

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="app">
     <NavBar />
-    <BookList />
+    <router-link to='/'>BookPage</router-link>
+    <router-view/>
+    <!-- <BookList /> -->
     <!-- <BookListFilter msg="Book List" /> -->
   </div>
 </template>
 
 <script>
-import BookListFilter from "./components/BookListFilter.vue";
-import NavBar from "./components/NavBar.vue";
-import BookList from "./components/BookList.vue";
+// import BookListFilter from "./components/BookListFilter.vue";
+// import NavBar from "./components/NavBar.vue";
+// import BookList from "./components/BookList.vue";
 
 export default {
-  name: "app",
-  components: {
-    BookListFilter,
-    NavBar,
-    BookList
-  }
+  name: "app"
+  // components: {
+  //   BookListFilter,
+  //   NavBar,
+  //   BookList
+  // }
 };
 </script>
 
--- src/components/AddPage.vue
+++ src/components/AddPage.vue
@@ -2,8 +2,10 @@
     <div class="container">
         <h1> Add Page </h1>
         <nav>
-            <router-link to='/addlanguage'>Add Language</router-link>
-            <router-link to='/addimage'>Add Image</router-link>
+            <tr>
+            <td><router-link to='/addlanguage'>Add Language</router-link></td>
+            <td><router-link to='/addimage'>Add Image</router-link></td>
+            </tr>
         </nav>
         <router-view/>
     </div>
