f9f0fb669c679d1493107bbbcc96977c7b8d57de
HbugLubas <harry.lubas@bellevuecollege.edu>
2019-03-03 20:21:07 -0800

    edited pages

--- src/components/AddImage.vue
+++ src/components/AddImage.vue
@@ -1,8 +1,17 @@
 <template>
     <div>
-        <b-jumbotron header="Add Image">
+        <b-card>
+        <b-jumbotron header="Add Image" lead='Select image file to upload'>
+        <hr />
+        <nav>
+            <ul>
+            <li><router-link to="">Add Files</router-link></li>
+            </ul>
+        </nav>
+        <hr />
         <router-link to='/' tag='button'>Back to book page</router-link>
         <router-view/>
         </b-jumbotron>
+        </b-card>
     </div>
 </template>
--- src/components/AddPage.vue
+++ src/components/AddPage.vue
@@ -1,14 +1,18 @@
 <template>
     <div>
-        <b-jumbotron header="Add Page">
+        <b-card>
+        <b-jumbotron header="Add Page" lead='Upload language audio and text, and image'>
+        <hr />
         <nav>
             <ul>
             <li><router-link to='/addlanguage'>Add Language</router-link></li>
             <li><router-link to='/addimage'>Add Image</router-link></li>
             </ul>
         </nav>
+        <hr />
         <router-link to='/' tag='button'>Back to book page</router-link>
         <router-view/>
         </b-jumbotron>
+        </b-card>
     </div>
 </template>
