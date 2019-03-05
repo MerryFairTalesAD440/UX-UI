e820c831b27ee359f32f8dbb6c6b795afe068153
gshields5541 <gshields5541@gmail.com>
2019-03-04 20:07:45 -0800

    First commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
-
+import dragdrop from "./components/dragdrop.vue";
 Vue.config.productionTip = false;
+
+new dragdrop({
+  render:h => h(dragdrop)
+}).$mount("#drapdrop");
 
 new Vue({
   render: h => h(App)
