0b41f8873bb4710b5b3ca7c7e96a130210862129
ncobb01 <34530495+ncobb01@users.noreply.github.com>
2019-03-05 13:39:45 -0800

    Update UploadImage.vue

--- src/components/UploadImage.vue
+++ src/components/UploadImage.vue
@@ -40,7 +40,7 @@
                 .then(response => {this.$session.set('sesh',response.data.token);})
                 .catch(error => (this.info = error))
               var myDate = new Date();
-              var myUrl = "https://testurl.com/AddImage/pic.jpg"+this.$session.get('sesh');
+              var myUrl = "https://nathansprint2.z13.web.core.windows.net/AddImage/pic.jpg"+this.$session.get('sesh');
               axios({ method: "PUT",
                   "url": myUrl,
                   "data": this.files,
