d85d4bc16975c6efc2baadf290824914b6bb082a
DnlBell <dnl.bell@gmail.com>
2019-03-04 18:00:02 -0800

    moving our src back (#51)

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div id="app">
     <NavBar />
-    <router-view/>
-    <!-- <BookList /> -->
+    <BookList />
     <!-- <BookListFilter msg="Book List" /> -->
   </div>
 </template>
 
 <script>
-// import BookListFilter from "./components/BookListFilter.vue";
-// import BookList from "./components/BookList.vue";
+import BookListFilter from "./components/BookListFilter.vue";
 import NavBar from "./components/NavBar.vue";
+import BookList from "./components/BookList.vue";
 
 export default {
   name: "app",
-   components: {
-         NavBar
-  //   BookListFilter,
-  //   BookList
+  components: {
+    BookListFilter,
+    NavBar,
+    BookList
   }
 };
 </script>
