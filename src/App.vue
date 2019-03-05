9818ecff66ffe89c11e33cbffaf3bffefdc34649
Jeremiah Smith <jsmit028@seattlecentral.edu>
2019-03-05 15:23:11 -0800

    fixing minor merge conflicts with dev

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div id="app">
     <NavBar />
-<<<<<<< HEAD
     <router-view />
-=======
-    <BookList />
->>>>>>> d85d4bc16975c6efc2baadf290824914b6bb082a
     <!-- <BookListFilter msg="Book List" /> -->
   </div>
 </template>
@@ -14,24 +10,17 @@
 import BookListFilter from "./components/BookListFilter.vue";
 import NavBar from "./components/NavBar.vue";
 import BookList from "./components/BookList.vue";
-<<<<<<< HEAD
 import AddBook from "./components/AddBook.vue";
 import UploadFile from "./components/UploadFile.vue";
-=======
->>>>>>> d85d4bc16975c6efc2baadf290824914b6bb082a
 
 export default {
   name: "app",
   components: {
     BookListFilter,
     NavBar,
-<<<<<<< HEAD
     BookList,
     AddBook,
     UploadFile
-=======
-    BookList
->>>>>>> d85d4bc16975c6efc2baadf290824914b6bb082a
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -3,14 +3,12 @@ import App from "./App.vue";
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-<<<<<<< HEAD
 import router from './router'
-=======
->>>>>>> d85d4bc16975c6efc2baadf290824914b6bb082a
 
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
