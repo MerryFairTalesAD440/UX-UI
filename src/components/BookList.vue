5ab9457ab555bb20ade4129739b95fb670797151
Samuel Richardson <toolbax@gmail.com>
2019-03-06 15:38:22 -0800

    Add Login.vue

---
+++ src/components/BookList.vue
@@ -0,0 +1,72 @@
+<template>
+   <!-- main content container     -->
+                    <b-container id="topPage" border-variant="dark">
+                        <b-row>
+                            <b-col>
+                            <!-- heading -->    
+                            <div>
+                            <hr class="my-4">
+                            <b-jumbotron>
+                                <template slot="header">
+                                    Admin Portal
+                                </template>
+                                <hr class="my-4">
+                                <b-btn variant="info" b-link to="AddBook">Add New Book</b-btn>
+                                            
+                            </b-jumbotron>
+                            </div>
+                            <!-- end heading -->
+        
+                            <!-- Cards to Display Books(getBooks) -->
+                            <div v-for="book in bookList" :key="book.id" >
+                                <b-card >
+                                    <h2 class="card-text">Title: {{ book.title }}</h2>
+                                    <h5 class="card-text">Author: {{ book.author }}</h5>
+                                    <h6 class="card-text">
+                                        Description: {{book.description}}
+                                    </h6>
+                                    <a href="#"
+                                       class="card-link">Edit Book</a>
+                                    <b-link href="#"
+                                            class="card-link">Delete Book</b-link>
+                                </b-card>
+                                <hr class="my-4">
+                            </div>
+                            <!-- End Book Cards -->
+                            </b-col>
+                        </b-row>
+
+                        <!-- Back to the Top(VueScrollTo) -->
+                        <a href="#" v-scroll-to="'#topPage'"><b>Back to the Top</b></a>
+                        <!-- <b-btn href="#" v-scroll-to="'#topPage'" variant="info">Back to the Top</b-btn> -->
+                        <hr class="my-4">
+                        <!-- End of Scroll To -->
+                        <hr class="my-4">
+
+                    </b-container>
+                    
+                    <!-- End Main Content Container -->
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name:'BookList',
+  data(){
+    return {
+      bookList: []
+    }
+  },
+   mounted () {
+    // var self = this
+    axios
+        .get('https://merry-fairy-tales.azurewebsites.net/api/v1/books')
+        .then(response => (this.bookList = response.data))
+        // .then(function (response) {
+        //     self.bookList = response.data
+        //     console.log('Data: ', response.data)
+        // })
+}
+}
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
 import Vue from "vue";
 import App from "./App.vue";
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import VueRouter from 'vue-router';
+import NavBar from "./components/NavBar.vue";
+import BookList from "./components/BookList.vue";
+import AddBook from "./components/AddBook.vue";
+import BookListFilter from "./components/BookListFilter.vue";
+import UploadFile from "./components//UploadFile.vue";
 
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', component: NavBar },
+  { path: '/BookListFilter', component: BookListFilter },
+  { path: '/BookList', component: BookList },
+  { path: '/AddBook', component: AddBook },
+  { path: '/UploadFile', component: UploadFile }
+];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
+
 new Vue({
+  el: '#app',
+  router,
   render: h => h(App)
-}).$mount("#app");
+});
