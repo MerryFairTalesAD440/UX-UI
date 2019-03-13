a674201d0fb473a1ed83239258367a6f5f42679a
Jeremiah Smith <jsmit028@seattlecentral.edu>
2019-03-12 19:11:19 -0700

    AddLanguage

---
+++ src/components/AddLanguage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <b-card>
+        <b-jumbotron header="Add Language" lead='Upload audio and/or text'>
+        <hr />
+        <nav>
+            <ul>
+            <li><router-link to="/uploadFile">Add Files</router-link></li>
+            </ul>
+        </nav>
+        <hr>
+        <router-link to='/bookpage' tag='button'>Back to book page</router-link>
+        <router-view/>
+        </b-jumbotron>
+        </b-card>
+    </div>
+</template>
